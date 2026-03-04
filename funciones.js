2993f6b844a898f181dac2c6d1b79a93af08d137
c99367315-hue <c99367315@gmail.com>
2026-03-04 15:35:31 -0500

    Funciones puras

---
+++ funciones.js
@@ -0,0 +1,13 @@
+function Suma(x) {
+    let accion = x + 5;
+    return accion;
+};
+
+console.log(Suma(7));
+
+function Division(x) {
+    let division = x / 2;
+    return division; 
+};
+
+console.log(Division(20));
