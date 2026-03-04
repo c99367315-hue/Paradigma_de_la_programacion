53fcc622a783420b95e772b95334b404824748ae
c99367315-hue <c99367315@gmail.com>
2026-03-04 16:11:34 -0500

    Funciones

--- funciones.js
+++ funciones.js
@@ -11,3 +11,31 @@ function Division(x) {
 };
 
 console.log(Division(20));
+
+let resultado = 0
+
+function SumarPrecios(a, b) {
+    return resultado = resultado + a * b;
+};
+
+console.log(SumarPrecios(4, 6));
+console.log(SumarPrecios(2, 3));
+console.log(SumarPrecios(5, 8));
+
+let vart = ""
+
+function Palabra(a) {
+    return vart = vart + a;
+};
+
+console.log(Palabra("Hola mundo"), Palabra(" como estas"));
+
+function Multiplicar(a) {
+    return a * a;
+};
+
+function SumaDeLaMultiplicacion(num1, num2) {
+    return Multiplicar(num1) + Multiplicar(num2);
+};
+
+console.log(SumaDeLaMultiplicacion(3, 4));
